3e70ac317b58ffafe654a6de880480c063c96890
Michael Socha <msocha@michael-caleb.com>
2025-05-09 23:26:18 -0500

    tuning

--- ui/src/app/cart/cart.component.ts
+++ ui/src/app/cart/cart.component.ts
@@ -1,23 +1,67 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { ShoppingCartService } from '../shared/shopping-cart-service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-cart',
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-  cartSvc: ShoppingCartService = inject(ShoppingCartService);
-  productSvc: ShoppingCartService = inject(ShoppingCartService);
+  readonly cartSvc: ShoppingCartService = inject(ShoppingCartService);
+  readonly productSvc: ShoppingCartService = inject(ShoppingCartService);
+  readonly dialog = inject(MatDialog);
+  readonly router = inject(Router);
 
-  confirmationDialog(): void {
-    this.cartSvc.clearCart();
-    alert('Your order has been placed successfully!');
+  checkout(): void {
+    let enterAnimationDuration = '1000ms';
+    let exitAnimationDuration = '1000ms';
+
+    this.dialog.open(CheckoutDialog, {
+      width: "250px",
+      enterAnimationDuration,
+      exitAnimationDuration
+    }).afterClosed().subscribe({
+      next: () => {
+        this.cartSvc.clearCart();
+        this.router.navigate(['/shop']);
+      }
+    });
   }
+
+  
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+  <h2 mat-dialog-title>Order confirmation #{{orderConfirmationNumber}}</h2>
+  <mat-dialog-content>
+    Thank you for your order. Your products will be shipped within 3-5 business days.
+  </mat-dialog-content>
+  <mat-dialog-actions>
+    <button mat-button mat-dialog-close cdkFocusInitial>Ok</button>
+  </mat-dialog-actions>
+  `
+})
+export class CheckoutDialog {
+  readonly dialogRef = inject(MatDialogRef<CheckoutDialog>);
+
+  orderConfirmationNumber: string = this.getOrderConfirmationNumber();
+
+  getOrderConfirmationNumber(): string {
+    return this.orderConfirmationNumber = (Math.floor(Math.random() * 1000000)).toString();
+  }
+
+  
 }
